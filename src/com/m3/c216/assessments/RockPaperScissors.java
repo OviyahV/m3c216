b8adf0f3b1c06ae03e1b58b9da10e254a0d3e6b2
OviyahV <oviyah.vai@gmail.com>
2022-04-27 23:06:52 +0100

    This program will be a Java Console Application called RockPaperScissors. The program must print out the number of ties, user wins, and computer wins and declare the overall winner based on who won more rounds.

--- src/com/m3/c216/assessments/RockPaperScissors.java
+++ src/com/m3/c216/assessments/RockPaperScissors.java
@@ -2,19 +2,16 @@ import java.util.*;
 
 public class RockPaperScissors {
     public static void main(String [] args){
-
-
-
         Scanner playersInput = new Scanner(System.in);
         System.out.print("How many rounds do you want to play? ");
         int totalRounds = playersInput.nextInt();
+
+
         int currentRounds = 0;
-        boolean playAgain = true;
         if(totalRounds>10 || totalRounds<1){
             System.out.println("Error: out of bounds");
         }
-
-        while(playAgain){
+        while(true){
             game();
             currentRounds++;
             if(currentRounds == totalRounds){
@@ -25,21 +22,9 @@ public class RockPaperScissors {
     }
 
 
-
-
-
     private static void game(){
 
-        Scanner Input = new Scanner(System.in);
-        System.out.println(" ");
-        System.out.printf("Time to choose Rock, Paper or Scissors: ");
-        String playersMove = Input.nextLine();
-        System.out.println("Player: " + playersMove);
-        if(!playersMove.equalsIgnoreCase("Rock") && !playersMove.equalsIgnoreCase("Paper") && !playersMove.equalsIgnoreCase("Scissors")){
-            System.out.println("Error");
-        }else{
-
-            // computers Move
+            // Computer's move
             Random random = new Random();
             int max = 4;
             int min = 1;
@@ -55,27 +40,42 @@ public class RockPaperScissors {
             }else {
                 compChoice = "Scissors";
             }
-            System.out.println("Computer: " + compChoice);
 
 
 
-            // winners and losers
+            // Player's move
+            Scanner Input = new Scanner(System.in);
+            System.out.printf("%n" + "Time to choose Rock, Paper or Scissors: ");
+            String playersMove = Input.nextLine();
+            System.out.printf("Player: " + playersMove + "%n" +"Computer: " + compChoice);
+            if(!playersMove.equalsIgnoreCase("Rock") && !playersMove.equalsIgnoreCase("Paper") && !playersMove.equalsIgnoreCase("Scissors")){
+                System.out.println("Error");
+            }else{
+
+
+
+            // Winners and Losers
             int compWins = 0;
             int playerWins = 0;
             int draws = 0;
 
             if(playersMove.equals(compChoice)){
                 draws++;
-                System.out.println("It's a TIE");
+                System.out.printf("%n" + "It's a TIE");
+
             }else if(playersMove.equals("Rock") && compChoice.equals("Scissors") || playersMove.equals("Paper") && compChoice.equals("Rock") || playersMove.equals("Scissors") && compChoice.equals("Paper")) {
                 playerWins++;
-                System.out.println("You WIN!");
+                System.out.printf("%n" + "You WIN!");
+
             }else{
                 compWins++;
+                System.out.printf("%n" + "You LOST!");
 
-                System.out.println("You LOST!");
             }
         }
         return;
     }
+
+
+
 }
